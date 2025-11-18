e80f55cb2af6d8e9d03669eb66d606c5aa24e5c8
yudiergg <gkscks1248@gmail.com>
2025-11-18 10:25:10 +0900

    ..
    ..

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('header .depth2').hide()
+    $('header .gnb .depth1 li a').on('mouseenter',function(){
+        $('header .depth2').show()
+    })
+    $('header .depth2').on('mouseleave',function(){
+    $('header .depth2').hide()
+    })
+
+    /* 슬라이드 함수 만들기
+    1. 먼저 변수 정하기 현재 보여지는거
+    2. 총 이미지
+    3. 이미지 갯수 */
+
+    var first = 0
+    var images = $('.visual ul li')
+    var total = images.length
+
+    images.hide().eq(0).show()
+    setInterval(function(){
+        images.eq(first).fadeOut(800)
+        first = (first + 1) % total
+        images.eq(first).fadeIn(800)
+    })
+})
