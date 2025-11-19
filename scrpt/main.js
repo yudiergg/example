6d01a11e5ddfb79d7f7aff4a614d0a6da82b5b5e
yudiergg <gkscks1248@gmail.com>
2025-11-19 11:22:10 +0900

    ..
    ..

--- scrpt/main.js
+++ scrpt/main.js
@@ -18,7 +18,24 @@ $(document).ready(function(){
         images.eq(first).fadeOut()
     },4000)
 
-    $('.bbs .news .tab li button').on('click', function(){
-        
+    $('.bbs .news .tab li.active button').show()
+    $('.bbs .news .tab .notice ul.active').show()
+    $(".tab > li > button").click(function () {
+    // 모든 detail ul 숨기기
+    $(".tab li ul.detail").removeClass("active");
+
+    // 클릭한 li의 detail ul만 보이기
+    $(this).siblings("ul.detail").addClass("active");
+
+    // li(active 상태) 스타일 변경을 위해 active 클래스도 li에 적용
+    $(".tab > li").removeClass("active");
+    $(this).parent("li").addClass("active");
+    });
+
+    $('.bbs .news .tab .notice ul li a').on('click', function(){
+        $('.popup .popupwrap').addClass('active')
+    })
+    $('.popup .popupwrap .btn').on('click', function(){
+        $('.popup .popupwrap').removeClass('active')
     })
 })
