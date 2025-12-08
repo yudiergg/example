bc990938eaa3087ba62b7f559377d90af6847e88
yudiergg <gkscks1248@gmail.com>
2025-12-08 14:30:08 +0900

    ..
    ..

---
+++ just 쇼핑몰/scrpt/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    // /*상하 슬라이드*/
+    // setInterval(function(){
+    //     $('.visual .wrapper ul li a').delay(2000).animate({'marginTop': '-300'})
+    //     $('.visual .wrapper ul li a').delay(2000).animate({'marginTop': '-600'})
+    //     $('.visual .wrapper ul li a').delay(2000).animate({'marginTop': '0'})
+    // })
+    /*좌우 슬라이드*/
+    // setInterval(function(){
+    //     $('.visual .wrapper ul li a').delay(1000).animate({'marginLeft': '-1200'})
+    //     $('.visual .wrapper ul li a').delay(1000).animate({'marginLeft': '-2400'})
+    //     $('.visual .wrapper ul li a').delay(1000).animate({'marginLeft': '0'})
+    // })
+})
---
+++ scrpt/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('header .gnb .depth1 li').on('mouseenter',function(){
+        $(this).children('header .gnb .depth1 .depth2').addClass('active')
+    })
+    $('header .gnb .depth1 li').on('mouseleave',function(){
+        $(this).children('header .gnb .depth1 .depth2').removeClass('active')
+    })
+    // setInterval(function(){
+    //     $('.contaier .visual ul li a').delay(2000).animate({'marginLeft': '-800'},1000)
+    //     $('.contaier .visual ul li a').delay(2000).animate({'marginLeft': '-1600'},1000)
+    //     $('.contaier .visual ul li a').delay(1000).animate({'marginLeft': '0'},1000)
+    // })
+    //     setInterval(function(){
+    //     $('.contaier .visual ul li a').delay(1000).animate({'marginTop': '-350'},1000)
+    //     $('.contaier .visual ul li a').delay(1000).animate({'marginTop': '-700'},1000)
+    //     $('.contaier .visual ul li a').delay(1000).animate({'marginTop': '0'},1000)
+    // })
+    $('.content .news button').on('click', function(){
+        const parent = $(this).parent('li')
+        if (parent.hasClass('notice')) {
+            $('.content .news .tab .notice').addClass('active')
+            $('.content .news .tab .galiery').removeClass('active')
+        } else {
+            $('.content .news .tab .galiery').addClass('active')
+            $('.content .news .tab .notice').removeClass('active')
+        }
+    })
+})
