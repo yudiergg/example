8f17a88a5184e2bc71c629522bff80741d3388bd
yudiergg <gkscks1248@gmail.com>
2025-12-10 17:29:28 +0900

    ..
    ..

---
+++ scrpt/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    // setInterval(function(){
+    //     $('.content .visual ul').delay(1000).animate({'marginLeft': '-870'}, 4000)
+    //     $('.content .visual ul').delay(1000).animate({'marginLeft': '-1740'}, 4000)
+    //     $('.content .visual ul').delay(1000).animate({'marginLeft': '0'}, 4000)
+    // })
+    // $('header .gnb .main li a').on('mouseenter', function(){
+    //     $(this).siblings('.sub').show(200)
+    // })
+    // $('header .gnb .main li a').on('mouseleave', function(){
+    //     $(this).siblings('.sub').hide(200)
+    // })
+    // $('header .gnb .main li').on('mouseenter', function(){
+    //     $(this).children('.sub').stop().slideDown()
+    // })
+    // $('header .gnb .main li').on('mouseleave', function(){
+    //     $(this).children('.sub').stop().slideUp()
+    // })
+    $('header .gnb .main li').on('mouseenter', function(){
+        $(this).find('.sub').show()
+    })
+    $('header .gnb .main li').on('mouseleave', function(){
+        $(this).find('.sub').hide()
+    })
+})
