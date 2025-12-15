75ac8fc1d2abdc66218e4438798afe1fb8b3fdb3
yudiergg <gkscks1248@gmail.com>
2025-12-15 14:11:19 +0900

    ..
    ..

--- just 쇼핑몰/scrpt/main.js
+++ just 쇼핑몰/scrpt/main.js
@@ -1,14 +1,39 @@
 $(document).ready(function(){
-    // /*상하 슬라이드*/
+    $('header .gnb .main li a').on('mouseenter', function(){
+        $('header .gnb .sub').slideDown(400)
+    })
+    $('header .gnb').on('mouseleave', function(){
+        $('header .gnb .sub').slideUp(400)
+    })
+
     // setInterval(function(){
-    //     $('.visual .wrapper ul li a').delay(2000).animate({'marginTop': '-300'})
-    //     $('.visual .wrapper ul li a').delay(2000).animate({'marginTop': '-600'})
-    //     $('.visual .wrapper ul li a').delay(2000).animate({'marginTop': '0'})
+    //     var images = $('.visual ul li a img')
+    //     images.delay(1000).animate({'marginLeft':'-1200'}, 1000)
+    //     images.delay(1000).animate({'marginLeft':'-2400'}, 1000)
+    //     images.delay(1000).animate({'marginLeft':'0'}, 1000)
     // })
-    /*좌우 슬라이드*/
     // setInterval(function(){
-    //     $('.visual .wrapper ul li a').delay(1000).animate({'marginLeft': '-1200'})
-    //     $('.visual .wrapper ul li a').delay(1000).animate({'marginLeft': '-2400'})
-    //     $('.visual .wrapper ul li a').delay(1000).animate({'marginLeft': '0'})
+    //     var images = $('.visual ul li a img')
+    //     images.delay(1000).animate({'marginTop':'-350'}, 1000)
+    //     images.delay(1000).animate({'marginTop':'-700'}, 1000)
+    //     images.delay(1000).animate({'marginTop':'0'}, 1000)
     // })
+
+    var first = 0
+    var images = $('.visual ul li a img')
+    var total = images.length
+
+    images.hide().eq(first).show()
+    setInterval(function(){
+        images.eq(first).fadeIn(800)
+        first = (first + 1) % total
+        images.eq(first).fadeOut(800)
+    }, 4000)
+
+    $('.news .notice .tab .detail li a').on('click', function(){
+        $('.modal').addClass('active')
+    })
+    $('.btn_close').on('click', function(){
+        $('.modal').removeClass('active')
+    })
 })
